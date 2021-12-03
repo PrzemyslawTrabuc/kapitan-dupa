65f990b463a6a3de9cb6636b5699d467e445548e
PrzemyslawTrabuc <czemek17@gmail.com>
2021-12-03 13:15:49 +0100

    Added kwiaty lotosu

--- src/components/GameBar.js
+++ src/components/GameBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './GameBar.scss'
+import KwiatyLotosu from './KwiatyLotosu'
 
 function GameBar(){
 
     return(
         <div className="GameBar">
         <div style={{backgroundColor: 'red'}}></div>
-        <div style={{backgroundColor: 'green', minWidth: "50%"}}></div>
+        <div style={{ minWidth: "50%"}}><KwiatyLotosu /></div>
         <div style={{backgroundColor: 'blue'}}></div>
         </div>
     )
---
+++ src/components/KwiatyLotosu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import kwiatyLotosuImg from '../images/kwiatyLotosu0.png'
+import './KwiatyLotosu.scss'
+
+
+function KwiatyLotosu(){
+
+    return(
+        <div id="KwiatyLotosu">
+        <img  src={kwiatyLotosuImg} alt="abc" />
+        </div>
+    )
+}
+
+export default KwiatyLotosu;
